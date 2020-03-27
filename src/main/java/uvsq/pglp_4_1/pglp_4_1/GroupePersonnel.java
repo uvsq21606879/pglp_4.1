dbc078ab6eac33fe9df7e29338b85c3469d59788
root <said.mohammed-seghir@ens.uvsq.fr>
2020-03-27 16:39:09 +0100

    Ajout du Groupe Personnel

---
+++ src/main/java/uvsq/pglp_4_1/pglp_4_1/GroupePersonnel.java
@@ -0,0 +1,25 @@
+package uvsq.pglp_4_1.pglp_4_1;
+
+import java.util.ArrayList;
+
+public class GroupePersonnel implements Hierarchie {
+	
+	private ArrayList<Hierarchie> Personnel = new ArrayList<Hierarchie>();
+	
+	
+	public void afficher() {
+		for(Hierarchie i : Personnel)
+			i.afficher();
+	}
+	
+	
+	public void ajouter(Hierarchie Personnel) {
+		this.Personnel.add(Personnel);
+	}
+	
+	
+	public void retirer(Hierarchie Personnel) {
+		this.Personnel.remove(Personnel);
+	}
+	
+}

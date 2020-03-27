ec61ed6184a51d0c7370b50902ded9eb1ad2e0f2
root <said.mohammed-seghir@ens.uvsq.fr>
2020-03-27 18:21:24 +0100

    Modification du Pom.xml

--- src/main/java/uvsq/pglp_4_1/pglp_4_1/ClassRun.java
+++ src/main/java/uvsq/pglp_4_1/pglp_4_1/ClassRun.java
@@ -3,58 +3,53 @@ package uvsq.pglp_4_1.pglp_4_1;
 import uvsq.pglp_4_1.pglp_4_1.Personnel.Builder;
 import java.time.LocalDate;
 
-
 public enum ClassRun {
-	
+
 	Entreprise;
-	
+
 	public void run() {
-		
-		Builder p1= new Builder("Said","seghir","devlopper",LocalDate.of(1997,4 ,10));
-		NumTel num1= new NumTel("fixe", "0681636879");
+
+		Builder p1 = new Builder("Said", "seghir", "devlopper", LocalDate.of(1997, 4, 10));
+		NumTel num1 = new NumTel("fixe", "0681636879");
 		p1.NumeroTel(num1);
-		Personnel devlopper=p1.build();
-		
-		
-		Builder p2= new Builder("camille","lelouche","secretaire",LocalDate.of(1994,5 ,1));
-		NumTel num2= new NumTel("telportable", "0771696279");
+		Personnel devlopper = p1.build();
+
+		Builder p2 = new Builder("camille", "lelouche", "secretaire", LocalDate.of(1994, 5, 1));
+		NumTel num2 = new NumTel("telportable", "0771696279");
 		p2.NumeroTel(num2);
-		Personnel secretarie=p2.build();
-		
-		
-		Builder p3= new Builder("Thomas","Muller","manager",LocalDate.of(1984,11 ,12));
-		NumTel num3= new NumTel("fixe", "0131436929");
+		Personnel secretarie = p2.build();
+
+		Builder p3 = new Builder("Thomas", "Muller", "manager", LocalDate.of(1984, 11, 12));
+		NumTel num3 = new NumTel("fixe", "0131436929");
 		p3.NumeroTel(num3);
-		Personnel manager=p3.build();
-		
-		Builder p4= new Builder("Maurice","Fabien","Chomeur",LocalDate.of(1994,1 ,28));
-		NumTel num4= new NumTel("telportable", "0661436924");
+		Personnel manager = p3.build();
+
+		Builder p4 = new Builder("Maurice", "Fabien", "Chomeur", LocalDate.of(1994, 1, 28));
+		NumTel num4 = new NumTel("telportable", "0661436924");
 		p4.NumeroTel(num4);
-		Personnel chomeur=p4.build();
-		
+		Personnel chomeur = p4.build();
+
 		GroupePersonnel Grp1 = new GroupePersonnel();
 		GroupePersonnel Grp2 = new GroupePersonnel();
 		GroupePersonnel Entreprise = new GroupePersonnel();
-		
-		
+
 		Entreprise.ajouter(devlopper);
 		Entreprise.ajouter(chomeur);
 		Entreprise.ajouter(manager);
 		Entreprise.ajouter(secretarie);
-		
-		
+
 		Grp1.ajouter(devlopper);
 		Grp1.ajouter(chomeur);
-		
+
 		Grp2.ajouter(manager);
 		Grp2.ajouter(secretarie);
-		
+
 		Entreprise.afficher();
-		
-		}
-	
-	public static void main (String arg[]) {
-		
+
+	}
+
+	public static void main(String arg[]) {
+
 		Entreprise.run();
 	}
 
--- src/main/java/uvsq/pglp_4_1/pglp_4_1/Personnel.java
+++ src/main/java/uvsq/pglp_4_1/pglp_4_1/Personnel.java
@@ -3,73 +3,70 @@ package uvsq.pglp_4_1.pglp_4_1;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
-public class Personnel implements Hierarchie  {
-	
+public class Personnel implements Hierarchie {
+
 	private final String Nom;
 	private final String Prenom;
 	private final String Fonction;
 	private final LocalDate DateNaissance;
 	private final ArrayList<NumTel> NumeroTel;
-	
-	
+
 	public void afficher() {
-		System.out.println(this.Nom + " " + this.Prenom + ": \nFonction: " 
-		+ this.Fonction +"  " +"Date de naissance: "+this.DateNaissance + "\n");	
+		System.out.println(this.Nom + " " + this.Prenom + ": \nFonction: " + this.Fonction + "  "
+				+ "Date de naissance: " + this.DateNaissance + "\n");
 	}
-	
-	public static class Builder{
-		
+
+	public static class Builder {
+
 		private final String Nom;
 		private final String Prenom;
 		private final String Fonction;
 		private final LocalDate DateNaissance;
 		private final ArrayList<NumTel> NumeroTel;
-		
-		
-		public Builder(String Nom, String Prenom, String Fonction, LocalDate DateNaissance ) {
-			this.Nom=Nom;
-			this.Prenom=Prenom;
-			this.Fonction=Fonction;
-			this.DateNaissance=DateNaissance;
-			this.NumeroTel= new ArrayList<NumTel>();
+
+		public Builder(String Nom, String Prenom, String Fonction, LocalDate DateNaissance) {
+			this.Nom = Nom;
+			this.Prenom = Prenom;
+			this.Fonction = Fonction;
+			this.DateNaissance = DateNaissance;
+			this.NumeroTel = new ArrayList<NumTel>();
 		}
-		
+
 		public Builder NumeroTel(NumTel numero) {
 			this.NumeroTel.add(numero);
 			return this;
 		}
-		
+
 		public Personnel build() {
 			return new Personnel(this);
 		}
 	}
-	
-	
+
 	private Personnel(Builder builder) {
-		this.Nom=builder.Nom;
-		this.Prenom=builder.Prenom;
-		this.Fonction=builder.Fonction;
-		this.DateNaissance=builder.DateNaissance;
-		this.NumeroTel=builder.NumeroTel;
+		this.Nom = builder.Nom;
+		this.Prenom = builder.Prenom;
+		this.Fonction = builder.Fonction;
+		this.DateNaissance = builder.DateNaissance;
+		this.NumeroTel = builder.NumeroTel;
 	}
-	
+
 	public String getNom() {
 		return this.Nom;
 	}
-	
+
 	public String getPrenom() {
 		return this.Prenom;
 	}
-	
+
 	public String getFonction() {
 		return this.Fonction;
 	}
-	
+
 	public LocalDate getDatDeNaissance() {
 		return this.DateNaissance;
 	}
-	
-	public ArrayList<NumTel> getNumerosTel(){
+
+	public ArrayList<NumTel> getNumerosTel() {
 		return this.NumeroTel;
 	}
 
